7071a2c9744ff9a93d3679d2cefc309d543bc50c
sonjoyb7u <sonjoy.john@gmail.com>
2026-09-02 10:33:39 +0600

    Question 2: Bangladesh Weekend Machine.

--- script.js
+++ script.js
@@ -7,5 +7,25 @@ function describeValue(any)
 {
     return `${typeof(any)} | ${any ? "truthy" : "falsy" }`;
 }
-
 // console.log(describeValue([]));
+
+/*
+* Question 2: Bangladesh Weekend Machine
+*/
+function getDayType(inputValue)
+{
+    inputValue = inputValue.charAt(0).toUpperCase() + inputValue.slice(1).toLowerCase();
+    switch(true)
+    {
+        case inputValue === "Sunday" || inputValue === "Monday" || inputValue === "Tuesday" || inputValue === "Wednesday" || inputValue === "Thursday":
+            return "Working Day";
+            break;
+        case inputValue === "Friday" || inputValue === "Saturday":
+            return "Weekend";
+            break;
+        default:
+            return "Invalid Day";
+            break;
+    }
+}
+// console.log(getDayType("JS"));
