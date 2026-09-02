d3f69aa82e7f06a6cc78f1c76a0289c2d02b2094
sonjoyb7u <sonjoy.john@gmail.com>
2026-09-02 10:10:49 +0600

    Question 1: Value Detective.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello JS Foundation Program!")
+/*
+* Question 1: Value Detective.
+* Falsy value = false, 0, -0, "", '', null, undefined, NaN
+* Truthy value = "string", " ", {}, [], "0"
+*/
+function describeValue(any)
+{
+    return `${typeof(any)} || ${any ? "truthy" : "falsy" }`;
+}
+
+// console.log(describeValue([]));
